e6fdc57267077519032e7199e7b163c38a2b5615
Arai Shota <s.arai@hkn.jp>
2025-05-07 23:13:12 +0900

    change

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now!!
           </a>
           <a
             href="https://turborepo.com/docs?utm_source"
